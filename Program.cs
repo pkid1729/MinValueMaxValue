6aee839b6d780c208a25142ce0c3e8580b5e13b2
pkid1729 <30475130+pkid1729@users.noreply.github.com>
2018-09-08 01:45:36 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinValueMaxValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} {1}",byte.MinValue,byte.MaxValue);
+            Console.WriteLine("{0} {1}",float.MinValue,float.MaxValue);
+            Console.WriteLine("{0} {1}",int.MinValue,int.MaxValue);
+
+        }
+    }
+}
